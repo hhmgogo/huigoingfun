4c966e5d19facd98bafd85ce0012e9d2574dbef8
周貳 <75374395+hhmgogo@users.noreply.github.com>
2022-06-05 22:54:52 +0800

    20220605 commit
    https://www.youtube.com/watch?v=WVOmmc0UTiM

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
+/*stylesheet*/
+const Blog = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding:40px;
+border-bottom:1px sold rgba(0,0,0,0.05);
+`;
+
+const Heading = styled.div`
+ display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const Span = styled.span`
+color: #f33c3c;
+`;
+
+const H3 = styled.h3`
+font-size:2.4rem; 
+color: #2b2b2b;
+font-weight: 600;
+`;
+
+const BlogCentainer = styled.div`
+  
+`;
+
+const BlogBox1 = styled.div`
+  
+`;
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Blog >
+        <Heading >
+          <Span> My Recent Posts </Span>
+          <H3 > My Blog </H3>
+        </Heading >
+      </Blog>
+
+    /*blog-container*/
+      <BlogCentainer>
+      //box1
+        <BlogBox1>
+
+        </BlogBox1>
+      </BlogCentainer>
     </div>
   );
 }
